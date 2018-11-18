f8c0922a4c1a940e2608cc96a783523d933e5458
brimo <bmosesso@gmail.com>
2018-11-17 23:18:59 -0700

    put update point totals in function

---
+++ dictionaries.py
@@ -0,0 +1,22 @@
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+#tarot code
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+
+tarot.pop(13)
+spread["past"] = "Death"
+
+tarot.pop(22)
+spread["present"] = "The Fool"
+
+tarot.pop(10)
+spread["future"] = "Wheel of Fortune"
+
+for card, fortune in spread.items():
+  print("Your " + card + " is the " + fortune + " card.")
+
+
+
--- scrabble.py
+++ scrabble.py
@@ -1,21 +1,36 @@
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
-
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " "]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10, 0]
 letter_to_points = {key:value for key, value in zip(letters, points)}
 #print(list(letter_to_points.items()))
 
-#refactor to add in lists above
-letter_to_points[" "] = 0
-print(list(letter_to_points.items()))
+#ask for a word
+word = input("What is your word? ")
 
+#calculate score of given word
 def score_word(word):
-  #get some input for a word
-  word = input("What is your word? ")
-  point_total = 0
-  for letter in word:
-    point_total += letter_to_points.get(letter.upper(), 0)
-  return point_total
+    point_total = 0
+    for letter in word:
+        point_total += letter_to_points.get(letter.upper(), 0)
+    return point_total
+print("Your point total for this word was " + str(score_word(word)) + ".")
 
-#test score_word function
-brownie_points = score_word("brownie")
-print(brownie_points)
+#test score_word function, should return 15
+#brownie_points = score_word("brownie")
+#print(brownie_points)
+
+#words played by each player
+player_to_words = {"player1": ["blue", "tennis", "exit"], "wordNerd": ["earth", "eyes", "machine"], "Lexi Con": ["eraser", "belly", "husky"], "Prof Reader": ["zap", "coma", "period"]}
+
+#update total points for each player
+def update_point_totals():
+    player_to_points = {}
+    for player, words in player_to_words.items():
+        player_points = 0
+        for word in words:
+            player_points += score_word(word)
+            player_to_points[player] = player_points
+    return player_to_points
+print(update_point_totals())
+
+
+
