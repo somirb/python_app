b073fedeb28c9e359d794569a11a0436ed677eb4
brian mosesso <bmosesso@gmail.com>
2018-11-28 12:33:45 -0700

    update variable name

--- turn_100.py
+++ turn_100.py
@@ -1,11 +1,11 @@
-name = input("What is your name? ")
+user_name = input("What is your name? ")
 current_age = int(input("What is your current age? "))
 
 def years_until():
     try:
         if current_age >= 0 and current_age <= 99:
             year = (100 - current_age) + 2018
-            return("Hi " + name + ", you will turn 100 years old in " + str(year))
+            return("Hi " + user_name + ", you will turn 100 years old in " + str(year))
         elif current_age >= 100:
             return("You're already 100!")
         elif current_age < 0:
