865e311991a7ece046a7fdc3d0901a9170eda463
brian mosesso <bmosesso@gmail.com>
2018-11-28 14:29:07 -0700

    add palindrone code

---
+++ string_lists_palindrone.py
@@ -0,0 +1,16 @@
+#using string reversal
+lst1 = input(str("Enter a string: "))
+lst2 = lst1[::-1]
+#print(lst2)
+if lst1 == lst2:
+    print("This is a palindrone.")
+else:
+    print("This is not a palindrone.")
+
+
+#refactored
+lst = input("Enter a string: ")
+if lst == lst[::-1]:
+    print("This is a palindrone.")
+else:
+    print("This is not a palindrone.")
