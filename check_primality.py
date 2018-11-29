26db4543ab8c2e24d0332ce9ec46b5e7055ebfa2
brimo <bmosesso@gmail.com>
2018-11-28 20:22:55 -0700

    delete file

--- check_primality.py
+++
@@ -1,7 +0,0 @@
-def check_primality():
-    user_num = int(input("Enter a number and I'll check if it's prime. \n"))
-    if user_num % 2 != 0:
-        print("Your number is not prime.")
-    else:
-        print("Your number is prime!")
-check_primality()
