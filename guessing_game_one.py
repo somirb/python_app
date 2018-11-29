a12e96a3a96e8627660f5c8f18be0431e81b090d
brimo <bmosesso@gmail.com>
2018-11-28 18:32:10 -0700

    modify guessing game to allow multiple guesses

--- guessing_game_one.py
+++ guessing_game_one.py
@@ -1,13 +1,21 @@
 import random
 rand_num = random.randint(1, 9)
 #print(rand_num)
-user_guess = int(input("Guess a number between 1 and 9. \n"))
+count = 0
+user_guess = 0
 
-def guessing_game():
-    if user_guess == rand_num:
-        print("Amazing! You guessed it!")
-    elif user_guess > rand_num:
-        print("Too high!")
+while user_guess != rand_num and user_guess != 'exit':
+    user_guess = input('Guess a number between 1 and 9. Type "exit" to end at any time. \n')
+    count += 1
+    if user_guess == 'exit':
+        break
+    user_guess = int(user_guess)
+    if user_guess > rand_num:
+         print("Too high!")
     elif user_guess < rand_num:
         print("Too low!")
-guessing_game()
+    else:
+        user_guess == rand_num
+        print("Amazing! You guessed it! It only took you " + str(count) + " tries.")
+
+    
